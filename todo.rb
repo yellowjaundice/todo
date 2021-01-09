e0d8868267392b9bc581807e204783344e2e9d29
Josh Larsen <larsen.bass@gmail.com>
2021-01-08 17:03:32 -0700

    initial commit

---
+++ todo.rb
@@ -0,0 +1,10 @@
+# user interface
+# add task, delete task, check off task, show tasks
+puts "Welcome Jaundice. Get to work!"
+def menu
+  puts "1. Add task"
+  puts "2. Remove task"
+  puts "3. Complete task"
+  puts "4. Exit"
+end
+menu
